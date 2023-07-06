bedc5baf1abf6ec79eaf98765ce4f58723ba15a4
Rodrigo GrauU <rodrigoassis1997@gmail.com>
2023-07-06 15:36:04 -0300

    234. OrderService Microservice - Configure Kafka Topic

---
+++ order-service/src/main/java/net/javaguides/orderservice/config/KafkaTopicConfig.java
@@ -0,0 +1,18 @@
+package net.javaguides.orderservice.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.config.TopicBuilder;
+
+public class KafkaTopicConfig {
+
+    @Value("${spring.kafka.topic.name}")
+    private String topicName;
+
+    @Bean
+    public NewTopic topic() {
+        return TopicBuilder.name(topicName)
+                .build();
+    }
+}
